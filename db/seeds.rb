ab4b1058dd8f775800f61dd6da315e8543a6e045
chikiriau-Olesya <faceremirabilia@gmail.com>
2020-10-18 16:32:38 +0300

    small change

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,20 @@ posts = [
 	author: "Nick Cave Old Too",
 	content: "Some Old Tracklist",
 	image: open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
+},
+{
+	name: "Old Album",
+	title: "The Bad Seeds Old",
+	author: "Nick Cave Old Too",
+	content: "Some Old Tracklist",
+	image: open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
+},
+{
+	name: "Old Album",
+	title: "The Bad Seeds Old",
+	author: "Nick Cave Old Too",
+	content: "Some Old Tracklist",
+	image: open("https://in-sound.ru/upload/iblock/d85/d859aa8387fd5fcd7a933be2af91066b.jpg")
 }
 ]
 
