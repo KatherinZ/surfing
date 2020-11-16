ea1d5d445a6ebfc305bf03b0cdb3eb46aeb4ee9a
KatherinZ <zyrianovakate10@gmail.com>
2020-11-16 14:49:36 +0300

    users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   http_basic_authenticate_with :name => "surfer", :password => "surferpassword", :except => [:index, :show]
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
@@ -27,7 +28,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @post.save
